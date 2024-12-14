bc7c39ae0781a1f799d6ce0364a856b4c41cb851
Sima Nouri Joybari <sima69_nj@yahoo.com>
2024-12-14 09:36:48 +0330

    new.task

---
+++ assest/js/main.js
@@ -0,0 +1,26 @@
+
+function obv(){
+  document.querySelector(".form-select-delet").style.display="block";
+
+ }
+ function obv1(){
+  document.querySelector(".form-select-delet").style.display="block";
+
+ }
+
+ function obv2(){
+  document.querySelector(".zabdar2").style.display="block";
+
+ }
+ function obv3(){
+  document.querySelector(".zabdar3").style.display="block";
+
+ }
+ function obv4(){
+  document.querySelector(".zabdar4").style.display="block";
+
+ }
+ function obv5(){
+  document.querySelector(".zabdar5").style.display="block";
+
+ }
