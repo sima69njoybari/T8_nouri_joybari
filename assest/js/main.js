fdc1983215f581fd3157879ffe08b339b4679b2a
Sima Nouri Joybari <sima69_nj@yahoo.com>
2024-12-16 23:16:50 +0330

    add new commit

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,26 +1,4 @@
-
 function obv(){
-  document.querySelector(".form-select-delet").style.display="block";
+  document.querySelector(".form-select-overed").style.display="block";
 
- }
- function obv1(){
-  document.querySelector(".form-select-delet").style.display="block";
-
- }
-
- function obv2(){
-  document.querySelector(".zabdar2").style.display="block";
-
- }
- function obv3(){
-  document.querySelector(".zabdar3").style.display="block";
-
- }
- function obv4(){
-  document.querySelector(".zabdar4").style.display="block";
-
- }
- function obv5(){
-  document.querySelector(".zabdar5").style.display="block";
-
- }
+}
